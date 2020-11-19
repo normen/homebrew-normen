bafb4ab89d822cf02c748a11df1abd796b4bbafe
normen <normen667@gmail.com>
2020-11-19 13:57:54 +0100

    initial commit

---
+++ Formula/whatscli.rb
@@ -0,0 +1,29 @@
+class Whatscli < Formula
+  desc "Command-line interface for WhatsApp"
+  homepage "https://github.com/normen/whatscli"
+  url "https://github.com/normen/whatscli/archive/v0.6.5.tar.gz"
+  sha256 "aaa7d4695608ec2600d3cfc49bbbad1fa5ff88674d6e066977486c579b8d2e6d"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "-o", "whatscli"
+    mkdir bin.to_s
+    cp "whatscli", "#{bin}/"
+  end
+
+  test do
+    #system "#{bin}/whatscli"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test whatscli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# Normen Tap
+
+
+## Included formulae
+- [whatscli](https://github.com/normen/whatscli)
+
+## How do I install these formulae?
+`brew install normen/tap/<formula>`
+
+Or `brew tap normen/tap` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
