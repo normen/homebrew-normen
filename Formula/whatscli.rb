eec3f72c164de09dc99dc1735f388592dc7f2ce1
Tripp Lilley <tripplilley@gmail.com>
2023-03-23 16:51:58 +0800

    ref(whatscli): remove extra stuff that std_go_args and build already do

--- Formula/whatscli.rb
+++ Formula/whatscli.rb
@@ -8,9 +8,7 @@ class Whatscli < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "-o", "whatscli"
-    mkdir bin.to_s
-    cp "whatscli", "#{bin}/"
+    system "go", "build", *std_go_args
   end
 
   test do
