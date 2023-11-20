fb2229ec6f5222df2b0d07c915e3a204ea1f436a
normen <normen667@gmail.com>
2023-11-20 15:24:35 +0100

    add obs-mcu

---
+++ Formula/obs-mcu.rb
@@ -0,0 +1,30 @@
+
+class ObsMcu < Formula
+  desc "Connect OBS and a Mackie Control Unit (MCU)"
+  homepage "https://github.com/normen/obs-mcu"
+  url "https://github.com/normen/obs-mcu/archive/v0.1.0.tar.gz"
+  sha256 "791185d0d2fd8e257bcf134e27eb1b585f0dbe847c8977a1feb91fe95c38cdb7"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "-o", "obs-mcu"
+    mkdir bin.to_s
+    cp "obs-mcu", "#{bin}/"
+  end
+
+  test do
+    #system "#{bin}/whatscli"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test whatscli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
